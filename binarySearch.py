e17119abadbe6165e96f3052527c5bab2c89b635
Chris Maina <krismaish34@gmail.com>
2017-06-16 08:39:58 +0300

    BinarySearch class
    Remove indentation and space errors

--- binarySearch.py
+++ binarySearch.py
@@ -1,37 +1,30 @@
 class BinarySearch(list):
-	
-	def __init__(self,len,step):
-		""" Constructor.
-		 Initializing the super class 
-		 """
-		 super(BinarySearch,self).__init__()
-          
-         #using len and step to populate class
-		 for number in range(1,len+1):
-		 	self.append(number*step)
+    def __init__(self, len, step):
+        """
+         Constructor.Initializing the super class
+        """
+        super(BinarySearch, self).__init__()
+        # using len and step to populate class
+        for number in range(1, len+1):
+            self.append(number*step)
+        # defining len attrib to be same as length of list from super class
+        self.len = len(self)
+    # defining search method that performs binary search.
+    # Number to find = searchNumber
 
-		 #defining len attrib to be same as length of list from super class
-		 self.len = len(self)
-
-	#defining search method that performs binary search. 
-	#Number to find = searchNumber
-	def search (self , searchNumber):
+    def search(self, searchNumber):
 		"""
-		Returns : dict of the form {'count':int,'index':int}
-		          index of the search value 
-		          and count to show binary iterations
-		"""
-
-
-		# first initialize the first and last indices plus count and index 
-		first = 0
-		last = len(self) - 1
-		val_index = 0
-		found = False
-		count = 0 
-
-		#checking if the search value is the 1st or last element
-		if searchNumber == self[first]:
+        Returns : dict of the form {'count':int,'index':int}
+        index of the search value
+        and count to show binary iterations
+        """
+        first = 0 # first initialize the first and last indices plus count and index
+        last = len(self) - 1
+        val_index = 0
+        found = False
+        count = 0 
+        #checking if the search value is the 1st or last element
+        if searchNumber == self[first]:
 			val_index = first
 			found = True
 		elif searchNumber == self[last]:
@@ -57,9 +50,3 @@ class BinarySearch(list):
 					first = mid_value + 1
 
 		return {'count':count , 'index': val_index}
-
-
-
-
-
-		
